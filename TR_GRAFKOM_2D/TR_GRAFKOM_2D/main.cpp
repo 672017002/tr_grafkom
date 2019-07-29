263cbfee0bc4b33472cdc7b842578eea19e25fe1
Andkurn <52996606+Andkurn@users.noreply.github.com>
2019-07-29 15:09:16 +0700

    mencoba membuat triangle
    saat menggunakan polygonshape, ingin di set vertexnya kena assertion error

--- TR_GRAFKOM_2D/TR_GRAFKOM_2D/main.cpp
+++ TR_GRAFKOM_2D/TR_GRAFKOM_2D/main.cpp
@@ -7,15 +7,49 @@
 #define PI 3.14
 const int size = 13;
 bool gravity = true;
+bool isTriangle = false;
 b2World* world;
 
-void addTriangle(int x, int y, int w, int h, bool dynamic){
+b2Body* addTriangle(int x, int y, bool dynamic){
+	b2BodyDef bodydef;
+	bodydef.position.Set(x, y);
+	if(dynamic){
+		bodydef.type = b2_dynamicBody;
+	}
+	b2Body* body = world->CreateBody(&bodydef);
+	b2FixtureDef fixturedef;
+	b2PolygonShape shape;
+	fixturedef.shape=&shape;
+	fixturedef.density=1.0;
+
+	//vertex untuk membuat segitiga
+	b2Vec2 vert[3];
+	vert[0] = b2Vec2(x-size, y-size);
+	vert[1] = b2Vec2(x+size, y-size);
+	vert[2] = b2Vec2(x, y+size);
+
+	int32 count = 3;
+	shape.Set(vert, count);
+
+	body->CreateFixture(&fixturedef);
+	return body;
 }
 
-void drawTriangle(){
+//drawTriangle belum dicoba, error masih di addTriangle shape.set assertion fail
+void drawTriangle(b2Vec2 points[3], b2Vec2 center, float angle){
+	glColor3f(1, 1, 1);
+	glPushMatrix();
+	glTranslatef(center.x, center.y, 0);
+	glRotatef(angle*180.0/PI, 0, 0, 1);
+	glBegin(GL_LINE_LOOP);
+	for(int a = 0; a < 3; a++){
+		glVertex2f(points[a].x, points[a].y);
+	}
+	glEnd();
+	glPopMatrix();
 }
 
-void addCircle(int x, int y, int radius, bool dynamic){
+b2Body* addCircle(int x, int y, int radius, bool dynamic){
 	b2BodyDef bodydef;
 	bodydef.position.Set(x, y);
 	if(dynamic){
@@ -32,6 +66,7 @@ void addCircle(int x, int y, int radius, bool dynamic){
 	fixturedef.shape=&shape;
 	fixturedef.density=1.0;
 	body->CreateFixture(&fixturedef);
+	return body;
 }
 
 void drawCircle(b2Vec2 center, float radius, float angle){
@@ -49,7 +84,7 @@ void drawCircle(b2Vec2 center, float radius, float angle){
 	glPopMatrix();
 }
 
-void addRect(int x, int y, int w, int h, bool dynamic){
+b2Body* addRect(int x, int y, int w, int h, bool dynamic){
 	b2BodyDef bodydef;
 	bodydef.position.Set(x, y);
 	if(dynamic){
@@ -64,6 +99,7 @@ void addRect(int x, int y, int w, int h, bool dynamic){
 	fixturedef.shape=&shape;
 	fixturedef.density=1.0;
 	body->CreateFixture(&fixturedef);
+	return body;
 }
 
 void drawRect(b2Vec2 points[4], b2Vec2 center, float angle){
@@ -78,12 +114,15 @@ void drawRect(b2Vec2 points[4], b2Vec2 center, float angle){
 	glEnd();
 	glPopMatrix();
 }
+
+
 void display(){
 	glClear(GL_COLOR_BUFFER_BIT);
 	glLoadIdentity();
 
 	b2Body* node = world->GetBodyList();
 	b2Vec2 points[4];
+	b2Vec2 trianglePoints[3];
 
 	while(node){
 		if(node->GetFixtureList()->GetShape()->GetType() == 0){
@@ -94,6 +133,11 @@ void display(){
 				points[a] = ((b2PolygonShape* ) node->GetFixtureList()->GetShape())->GetVertex(a);
 			}
 			drawRect(points, node->GetWorldCenter(), node->GetAngle());
+		}
+		if(isTriangle){
+			for(int a = 0; a < 3; a++){
+			}
+			isTriangle = false;
 		}
 		node = node->GetNext();
 	}
@@ -107,7 +151,40 @@ void loop(int v){
 		glutTimerFunc(1, loop, 0);
 }
 
-void keyFunc(unsigned char key, int x, int y){
+b2Body* myRect;
+void init(void){
+	glClearColor(0, 0, 0, 0);
+    glMatrixMode(GL_PROJECTION);
+	gluOrtho2D(0.0, 800.0, 0.0, 600.0);
+	glMatrixMode(GL_MODELVIEW);
+	glClearColor(0,0,0,1);
+	world = new b2World(b2Vec2(0, -9.81));
+	glColor3f(1, 1, 1);
+	addRect(400, -5, 800, 10, false);
+	//myRect = addRect(300, 300, size, size, true);
+
+}
+bool mouseDown = false;
+void mouseFunc(int button, int state, int x, int y){
+	x = x;
+	y = 600 - y;
+		if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+		{
+			mouseDown = true;
+		}else{
+			mouseDown = false;
+		}
+}
+
+void motionFunc(int x, int y){
+	if(mouseDown){
+		//printf("asdasd");
+		b2Vec2 mouse(x, 600-y);
+		myRect->ApplyForceToCenter(mouse, true);
+	}
+}
+
+void keyboardFunc(unsigned char key, int x, int y){
 	x = x;
 	y = 600 - y;
 	switch(key){
@@ -121,6 +198,8 @@ void keyFunc(unsigned char key, int x, int y){
 		break;
 	case 'D':
 	case 'd':
+		isTriangle = true;
+		addTriangle(x, y, true);
 		break;
 	case 'F':
 	case 'f':
@@ -132,21 +211,8 @@ void keyFunc(unsigned char key, int x, int y){
 	case 27:
 		exit(1);
 	}
-	world->Step(0.01, 5, 5); //redisplay versi box2d
 	glutPostRedisplay();
 }
-
-void init(void){
-	glClearColor(0, 0, 0, 0);
-    glMatrixMode(GL_PROJECTION);
-	gluOrtho2D(0.0, 800.0, 0.0, 600.0);
-	glMatrixMode(GL_MODELVIEW);
-	glClearColor(0,0,0,1);
-	world = new b2World(b2Vec2(0, -9.81));
-	glColor3f(1, 1, 1);
-	addRect(400, -5, 800, 10, false);
-}
-
 int main(int argc, char** argv){
 	printf("PRESS F TO ENABLE GRAVITY\n\n");
 	glutInit(&argc, argv);
@@ -159,7 +225,9 @@ int main(int argc, char** argv){
 	init();
 
 	glutDisplayFunc(display);
-	glutKeyboardFunc(keyFunc);
+	glutMouseFunc(mouseFunc);
+	glutMotionFunc(motionFunc);
+    glutKeyboardFunc(keyboardFunc);
 
 	glutMainLoop();
 	return 0;
